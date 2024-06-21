876ef9168aae56b3c383a38955500f8d98e42ada
dave-ad <davidaderibigbe47@gmail.com>
2024-06-21 03:38:16 +0100

    + Create group

--- ThriftManager.Domain/GlobalUsing.cs
+++ ThriftManager.Domain/GlobalUsing.cs
@@ -1,4 +1,5 @@
 ﻿global using ThriftManager.Domain.Core;
+global using ThriftManager.Domain.Entities;
 global using ThriftManager.Domain.ValueObjects;
 global using ThriftManager.Utils.Enums;
 global using ThriftManager.Utils;
--- ThriftManager.Service/GroupServices/IGroupService.cs
+++ ThriftManager.Service/GroupServices/IGroupService.cs
@@ -3,5 +3,6 @@
 public interface IGroupService
 {
     Task<ServiceResponse<GroupIdResponse>> CreateGroup(CreateGroupRequest request);
+    Task<ServiceResponse<GroupIdResponse>> JoinGroup(int memberId, int groupId);
     //Task<ServiceResponse<IEnumerable<GroupResponse>>> ViewAllGroups();
 }
